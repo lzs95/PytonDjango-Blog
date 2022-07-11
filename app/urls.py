a78b2e5dd9e5881a5fcfc8a4ae6f664d2192dee0
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-07-11 19:31:31 +0900

    feat: hide secret key and start work on auth

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . views import PostView
+from rest_framework_simplejwt.views import (TokenRefreshView,TokenObtainPairView)
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('posts/', PostView.as_view(), name ="post_view")
 ]
--- d_rest/settings.py
+++ d_rest/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
-
+# Hide Secret DjangoKEY
+from dotenv import load_dotenv
+import os
+load_dotenv()
+##
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tsxw!1phmo#&b3v%po@s!ny8r7hxf+8qm!eo6b2z3-v2)d*75l'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -26,7 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    "corsheaders"
+    'corsheaders',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -97,8 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -118,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
